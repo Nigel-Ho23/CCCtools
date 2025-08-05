afc81a801693f7041fd5b609539b05760e9f3eff
Nigel Ho <nigelho23@gmail.com>
2025-08-05 16:31:54 +0800

    minor edit

--- R/test.R
+++ R/test.R
@@ -18,7 +18,7 @@ options(reticulate.conda_binary = "C:/Users/hojkn/AppData/Local/miniforge3/conda
 conda_binary()
 
 # Create CellPhoneDB environment
-conda_create(envname = "cpdb", environment = "cpdb.yaml")
+# conda_create(envname = "cpdb", environment = "cpdb.yaml")
 
 # Select python for analysis
 python_binary <- conda_list() %>%
@@ -30,60 +30,18 @@ python_binary
 
 use_python(python_binary, required = TRUE)
 
+pacman::p_load_gh("Nigel-Ho23/CCCtools")
 
-# Run CellPhoneDB ---------------------------------------------------------
+test <- run_cellphonedb(seurat_obj,
+                        condition = "condition",
+                        toKeep    = "NL",
+                        cpdbPath  = "../data/cellphonedb.zip",
+                        metaPath  = "../data/NL_meta.tsv",
+                        adataPath = "../data/NL.h5ad",
+                        outPath   = "results/cpdb/NL")
 
-ad <- import("anndata")
-cpdb.analysis <- import("cellphonedb.src.core.methods.cpdb_statistical_analysis_method")
 
-NL.meta <- seurat_obj@meta.data %>%
-  filter(condition == "NL") %>%
-  rownames_to_column(var = "barcode") %>%
-  select(barcode, labels) %>%
-  write_delim(file = "../data/NL_meta.tsv", delim = "\t") # save file for later use
+help(run_cellphonedb)
 
-NL <- subset(seurat_obj, condition == "NL")
 
-NL.normcounts <- NL[["RNA"]]$data
 
-identical(NL.meta$barcode, colnames(NL.normcounts))
-
-NL.genes   <- data.frame(gene = rownames(NL.normcounts)) %>%
-  column_to_rownames("gene")
-
-NL.barcode <- data.frame(barcode = colnames(NL.normcounts)) %>%
-  column_to_rownames("barcode")
-
-adata <- ad$AnnData(X   = NL.normcounts,
-                    obs = NL.genes,
-                    var = NL.barcode)
-
-adata <- adata$transpose()
-
-adata$write_h5ad("../data/NL.h5ad")
-
-# Main database containing the interaction
-download.file("https://raw.github.com/ventolab/cellphonedb-data/refs/heads/master/cellphonedb.zip",  "../data/cellphonedb.zip")
-
-cpdb_file_path   <- "../data/cellphonedb.zip"
-meta_file_path   <- "../data/NL_meta.tsv"
-counts_file_path <- "../data/NL.h5ad"
-out_path         <- "results/cpdb/NL"
-
-dir.create(out_path, recursive = TRUE)
-
-cpdb_results <- cpdb.analysis$call(
-  cpdb_file_path = cpdb_file_path,
-  meta_file_path = meta_file_path,
-  counts_file_path = counts_file_path,
-  counts_data = 'hgnc_symbol',
-  # active_tfs_file_path = active_tf_path,
-  # microenvs_file_path = microenvs_file_path,
-  output_path = out_path
-)
-
-"NL"
-"../data/cellphonedb.zip"
-"../data/NL_meta.tsv"
-"../data/NL.h5ad"
-"results/cpdb/NL"

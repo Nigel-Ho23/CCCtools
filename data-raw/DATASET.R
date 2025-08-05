de44fd0273c7aa50e43861fcf5b4b448437c0e99
Nigel Ho <nigelho23@gmail.com>
2025-08-05 13:13:14 +0800

    Added download CellChat tutorial file

--- data-raw/DATASET.R
+++
@@ -1,8 +0,0 @@
-## code to prepare `DATASET` dataset goes here
-
-set.seed(123)
-testdata <- rnorm(1000)
-usethis::use_data(testdata, overwrite = TRUE)
-
-my_iris <- iris[1:50, ]
-usethis::use_data(my_iris, overwrite = TRUE)

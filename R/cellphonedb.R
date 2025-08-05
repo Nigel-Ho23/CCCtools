525ed267407d1fcaeae748926a02f42bbc0dbc61
Nigel Ho <nigelho23@gmail.com>
2025-08-05 12:15:07 +0800

    Added man to cellphonedb

--- R/cellphonedb.R
+++ R/cellphonedb.R
@@ -1,3 +1,11 @@
+#' Run CellPhoneDB version 5 in R
+#'
+#' @param obj Seurat object
+#' @param ...
+#'
+#' @returns Folder path
+#' @export
+#'
 run_cellphonedb <- function(obj, ...){
 
   print("done")
